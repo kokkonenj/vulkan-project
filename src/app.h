5931ee77651258416b50f5e1fce33e69b6e3bb9a
kokkonenj <jesse.kokkonen1@gmail.com>
2023-01-24 03:59:14 +0200

    defined app header

--- src/app.h
+++ src/app.h
@@ -1,6 +1,18 @@
 #pragma once
 
-#include "vk_types.h"
+#include <vk_types.h>
 
 class App {
+public:
+	bool isInitialized{ false };
+	int frameNumber{ 0 };
+
+	VkExtent2D windowExtent{ 800, 600 };
+
+	struct SDL_window* window{ nullptr };
+
+	void init();
+	void cleanup();
+	void draw();
+	void run();
 };
