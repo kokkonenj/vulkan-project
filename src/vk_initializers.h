c46a1c8de8da79e8845896e9afb648f0a6f59651
kokkonenj <jesse.kokkonen1@gmail.com>
2023-02-07 07:53:24 +0200

    initializers for the pipeline

---
+++ src/vk_initializers.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vk_types.h>
+
+namespace VkInit {
+	VkPipelineShaderStageCreateInfo pipelineShaderStageCreateInfo(VkShaderStageFlagBits stage, VkShaderModule shaderModule);
+	VkPipelineVertexInputStateCreateInfo vertexInputStateCreateInfo();
+	VkPipelineInputAssemblyStateCreateInfo inputAssemblyCreateInfo(VkPrimitiveTopology topology);
+	VkPipelineRasterizationStateCreateInfo rasterizationStateCreateInfo(VkPolygonMode polygonMode);
+	VkPipelineMultisampleStateCreateInfo multisamplingStateCreateInfo();
+	VkPipelineColorBlendAttachmentState colorBlendAttachmentState();
+}
---
+++ src/vk_pipeline.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <vk_types.h>
+
+class PipelineBuilder
+{
+public:
+	std::vector<VkPipelineShaderStageCreateInfo> shaderStages;
+	VkPipelineVertexInputStateCreateInfo vertexInputInfo;
+	VkPipelineInputAssemblyStateCreateInfo inputAssembly;
+	VkViewport viewport;
+	VkRect2D scissor;
+	VkPipelineRasterizationStateCreateInfo rasterizer;
+	VkPipelineColorBlendAttachmentState colorBlendAttachment;
+	VkPipelineMultisampleStateCreateInfo multisampling;
+	VkPipelineLayout pipelineLayout;
+
+	VkPipeline buildPipeline(VkDevice device, VkRenderPass pass);
+};
